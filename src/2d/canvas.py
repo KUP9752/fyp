578064ad65a620c29c21a1018970436ac01a54c7
Kagan Ugur Pekgoz <kaganpekgoz@gmail.com>
2024-11-24 11:03:52 +0000

    extremely primitive way to predict movement, does not work well

--- src/2d/canvas.py
+++ src/2d/canvas.py
@@ -1,82 +1,183 @@
 import pygame
 from pygame.color import Color
 
-
 import pickle
 import random
 
 
-pygame.init()
-clock = pygame.time.Clock()
+## File to save the demonstration data to train on
+def play_game_to_teach(filepath: str) -> None:
 
-## Setup Screen
-WIDTH, HEIGHT = 800, 600
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("2D Canvas")
+  pygame.init()
+  clock = pygame.time.Clock()
+
+  ## Setup Screen
+  WIDTH, HEIGHT = 800, 600
+  screen = pygame.display.set_mode((WIDTH, HEIGHT))
+  pygame.display.set_caption("2D Canvas")
 
 
 
-MOV_SPEED = 5
+  MOV_SPEED = 5
 
-BLOCK_SIZE = 20 # per side
+  BLOCK_SIZE = 20 # per side
 
-xBound = WIDTH - BLOCK_SIZE
-yBound = HEIGHT - BLOCK_SIZE
+  xBound = WIDTH - BLOCK_SIZE
+  yBound = HEIGHT - BLOCK_SIZE
 
-agent = pygame.Rect(random.randint(0, xBound), random.randint(0, yBound), BLOCK_SIZE, BLOCK_SIZE)
-target = pygame.Rect(random.randint(0, xBound), random.randint(0, yBound), BLOCK_SIZE, BLOCK_SIZE)
+  agent = pygame.Rect(random.randint(0, xBound), random.randint(0, yBound), BLOCK_SIZE, BLOCK_SIZE)
+  target = pygame.Rect(random.randint(0, xBound), random.randint(0, yBound), BLOCK_SIZE, BLOCK_SIZE)
 
-isRunning = True
+  isRunning = True
 
-movement: dict = {
-  pygame.K_UP: (0, -MOV_SPEED),
-  pygame.K_DOWN: (0, MOV_SPEED),
-  pygame.K_LEFT: (-MOV_SPEED, 0),
-  pygame.K_RIGHT: (MOV_SPEED, 0)
-}
+  movement: dict = {
+    pygame.K_UP: (0, -MOV_SPEED),
+    pygame.K_DOWN: (0, MOV_SPEED),
+    pygame.K_LEFT: (-MOV_SPEED, 0),
+    pygame.K_RIGHT: (MOV_SPEED, 0)
+  }
 
-data = []
+  data = []
 
-while isRunning:
-  ## White Background
-  screen.fill(Color("white"))
-    
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      isRunning = False
-    if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-      with open("demonstration.pkl", "wb") as f:
-        pickle.dump(data, f)
+  while isRunning:
+    ## White Background
+    screen.fill(Color("white"))
+      
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
         isRunning = False
-        
-  ## Game Logic
-  ## draw the squares, agend is BLUE, target is RED
-  pygame.draw.rect(screen, Color("blue"), agent)
-  pygame.draw.rect(screen, Color("red"), target)
-  
-  
-  ## record the action given to the robot in this coord system
-  state = agent.x, agent.y, target.x, target.y
-  action = {pygame.K_UP: False, pygame.K_DOWN: False, pygame.K_LEFT: False, pygame.K_RIGHT:  False }
-  
-  keys = pygame.key.get_pressed()
-  for key, (dx, dy) in movement.items():
-    if keys[key]:
-      action[key] = True
-      agent.move_ip(dx, dy)
-  #     agent.move_ip(dx, dy) # this is a move 'in-place', doesn't alter the object
-  
-  
-  ## When collided restart the target, so the game continuosly runs
-  if agent.colliderect(target):
-    target.x = random.randint(0, xBound)
-    target.y = random.randint(0, yBound)
-  
+      if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+        with open(filepath, "wb") as f:
+          pickle.dump(data, f)
+          isRunning = False
+          
+    ## Game Logic
+    ## draw the squares, agend is BLUE, target is RED
+    pygame.draw.rect(screen, Color("blue"), agent)
+    pygame.draw.rect(screen, Color("red"), target)
     
-  ## save the data from this frame  
-  data.append((state, action))
-  
-  pygame.display.update()
-  clock.tick(60)
+    
+    ## record the action given to the robot in this coord system
+    state = agent.x, agent.y, target.x, target.y
+    action = {pygame.K_UP: False, pygame.K_DOWN: False, pygame.K_LEFT: False, pygame.K_RIGHT:  False }
+    
+    keys = pygame.key.get_pressed()
+    for key, (dx, dy) in movement.items():
+      if keys[key]:
+        action[key] = True
+        agent.move_ip(dx, dy)
+    #     agent.move_ip(dx, dy) # this is a move 'in-place', doesn't alter the object
+    
+    
+    ## When collided restart the target, so the game continuosly runs
+    if agent.colliderect(target):
+      target.x = random.randint(0, xBound)
+      target.y = random.randint(0, yBound)
+    
+      
+    ## save the data from this frame  
+    data.append((state, tuple(action.values())))
+    
+    pygame.display.update()
+    clock.tick(60)
 
-pygame.quit()
+  pygame.quit()
+  
+  
+  
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+## Train a model on teh demonstration data
+def learn_game(filepath: str) -> RandomForestClassifier:
+  
+  with open(filepath, "rb") as f:
+    data = pickle.load(f)
+  
+  
+  print(f"{data = }")
+  
+  X, y = zip(*data) # states, (demonstrated) actions
+
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=12) ## random state shuffles the data
+
+  model = RandomForestClassifier()
+  model.fit(X_train, y_train)
+
+  y_pred = model.predict(X_test)
+  
+  print(accuracy_score(y_test, y_pred))
+  return model
+  
+
+def play_game(model: RandomForestClassifier) -> None:
+  pygame.init()
+  clock = pygame.time.Clock()
+
+  ## Setup Screen
+  WIDTH, HEIGHT = 800, 600
+  screen = pygame.display.set_mode((WIDTH, HEIGHT))
+  pygame.display.set_caption("2D Canvas")
+
+  MOV_SPEED = 5
+
+  BLOCK_SIZE = 20 # per side
+
+  xBound = WIDTH - BLOCK_SIZE
+  yBound = HEIGHT - BLOCK_SIZE
+
+  agent = pygame.Rect(random.randint(0, xBound), random.randint(0, yBound), BLOCK_SIZE, BLOCK_SIZE)
+  target = pygame.Rect(random.randint(0, xBound), random.randint(0, yBound), BLOCK_SIZE, BLOCK_SIZE)
+
+  isRunning = True
+
+  movement: dict = {
+    pygame.K_UP: (0, -MOV_SPEED),
+    pygame.K_DOWN: (0, MOV_SPEED),
+    pygame.K_LEFT: (-MOV_SPEED, 0),
+    pygame.K_RIGHT: (MOV_SPEED, 0)
+  }
+
+  while isRunning:
+    ## White Background
+    screen.fill(Color("white"))
+      
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        isRunning = False
+          
+    ## Game Logic
+    ## draw the squares, agend is BLUE, target is RED
+    pygame.draw.rect(screen, Color("blue"), agent)
+    pygame.draw.rect(screen, Color("red"), target)
+    
+    state = agent.x, agent.y, target.x, target.y
+    
+    pred = model.predict([state])[0]
+    
+    
+    action = {key: movement for key, movement in zip(movement.keys(), pred)}
+    print(f"{action = }")
+    
+    
+    for key, (dx, dy) in movement.items():
+      if action[key]:
+        agent.move_ip(dx, dy)
+    
+    ## When collided restart the target, so the game continuosly runs
+    if agent.colliderect(target):
+      target.x = random.randint(0, xBound)
+      target.y = random.randint(0, yBound)
+    
+    pygame.display.update()
+    clock.tick(60)
+
+  pygame.quit()
+
+
+# play_game_to_teach("demonstration.pkl")
+model = learn_game("demonstration.pkl")
+play_game(model)
+
+
