da17ad4ec9e97261c0bdcc716c46443e1916d49f
Kagan Ugur Pekgoz <kaganpekgoz@gmail.com>
2024-11-25 17:08:42 +0000

    trained a model on 10k data, kinda similar results

--- src/2d/model_and_play.py
+++ src/2d/model_and_play.py
@@ -4,9 +4,9 @@ import pickle
 
 if __name__ == "__main__":
   # trainingData = learn_game(agentMovement = human_interaction, n = 10)
-  with open("./src/2d/datasets/1k-targets.pkl", "rb") as f:
+  with open("./src/2d/datasets/10k-targets.pkl", "rb") as f:
     data = pickle.load(f)
     
-  model = train_on_behaviour(data = data)
-  play_game(model = model)
+  model = train_on_behaviour(modelName = "agent-network-10k.pth", data = data, overwriteDevice="cpu")
+  play_game(model)
     
