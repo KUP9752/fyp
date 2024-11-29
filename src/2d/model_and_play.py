5766aedd686e50fe4d0e4e7e6c756496250fa9df
Kagan Ugur Pekgoz <kaganpekgoz@gmail.com>
2024-11-29 14:28:56 +0000

    model is set to defualt play the game

--- src/2d/model_and_play.py
+++ src/2d/model_and_play.py
@@ -4,9 +4,10 @@ import pickle
 
 if __name__ == "__main__":
   # trainingData = learn_game(agentMovement = human_interaction, n = 10)
-  with open("./src/2d/datasets/10k-targets.pkl", "rb") as f:
-    data = pickle.load(f)
+  # with open("./src/2d/datasets/1k-targets.pkl", "rb") as f:
+  #   data = pickle.load(f)
     
-  model = train_on_behaviour(modelName = "agent-network-10k.pth", data = data, overwriteDevice="cpu")
-  play_game(model)
-    
+  # model = train_on_behaviour(modelName = "agent-network-750.pth", data = data[:750], overwriteDevice="cpu")
+  # play_game(loadModelFromFile="./src/2d/models/agent-network-1k.pth")    
+  # learn_game(n = 10)
+  play_game(loadModelFromFile="./src/2d/models/agent-network-1k.pth")
