69b9b2b1c41f74357c83d4b8ac5fb0d91747d603
Kagan Ugur Pekgoz <kaganpekgoz@gmail.com>
2025-05-04 22:15:37 +0100

    added layer sizes after making the diagram for report

--- src/3d/kup-bench/modules/multi_cam_cnn.py
+++ src/3d/kup-bench/modules/multi_cam_cnn.py
@@ -19,18 +19,23 @@ class CNNEncoder(nn.Module):
   def __init__(self):
     super(CNNEncoder, self).__init__()
     self.conv_encode = nn.Sequential(
+      # 3 64 64
       nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=0),
       nn.MaxPool2d(kernel_size=(2, 2), stride=2, padding=0),
       nn.ReLU(inplace=False),
+      # 32 31 31 
       nn.Conv2d(in_channels=32, out_channels=48, kernel_size=3, stride=1, padding=0),
       nn.MaxPool2d(kernel_size=(2, 2), stride=2, padding=0),
       nn.ReLU(inplace=False),
+      # 48 14 14
       nn.Conv2d(in_channels=48, out_channels=64, kernel_size=3, stride=1, padding=0),
       nn.MaxPool2d(kernel_size=(2, 2), stride=2, padding=0),
       nn.ReLU(inplace=False),
+      # 64 6 6
       nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=0),
       nn.MaxPool2d(kernel_size=(2, 2), stride=2, padding=0),
       nn.ReLU(inplace=False),
+      # 128 2 2
     )
     
     
