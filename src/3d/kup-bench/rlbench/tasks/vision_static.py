ef3717b274d7d0bc6a7771f3dabb56e944e3ae24
Kagan Ugur Pekgoz <kaganpekgoz@gmail.com>
2025-05-08 14:29:38 +0100

    trialing different distances etc, add distance as a constructor to task later for easier experimentation

--- src/3d/kup-bench/rlbench/tasks/vision_static.py
+++ src/3d/kup-bench/rlbench/tasks/vision_static.py
@@ -2,9 +2,11 @@ from typing import List, Optional
 from rlbench.backend.task import Task
 from pyrep.objects.proximity_sensor import ProximitySensor
 from pyrep.objects.shape import Shape
+from pyrep.objects.vision_sensor import VisionSensor
 from rlbench.backend.conditions import DetectedCondition
 from rlbench.const import colors as colours
 from pyrep.objects import Object
+
 from rlbench.backend.spawn_boundary import SpawnBoundary
 
 from pyrep import PyRep
@@ -14,6 +16,11 @@ from rlbench.backend.robot import Robot
 import numpy as np
 MAX_SCALE = 1.3 ## anything larger cannot be grasped
 MIN_SCALE = 0.3 ## arbitrary didn't want it too small
+
+
+## from wrist_cam
+MAX_Z_DIST = 0.8 ## not sure fully try these
+MIN_Z_DIST = 0.3 ## need to get closer for experiments??
 
 class VisionStatic(Task):
   
@@ -29,6 +36,8 @@ class VisionStatic(Task):
     def init_task(self) -> None:
       self.grasp_target = Shape("grasp_target")
       self.grasp_target_visual = Shape("grasp_cube")
+      self.wrist_cam = VisionSensor("cam_wrist")
+      
       # success_sensor =  ProximitySensor("success")
       self.register_graspable_objects([self.grasp_target])
       self.boundary = Shape("boundary")
@@ -49,8 +58,12 @@ class VisionStatic(Task):
         self.picked_scale,
         self.picked_scale
       )
+      self.grasp_target.set_position(
+        [0, 0, 0.25],
+        relative_to=self.wrist_cam
+      )
       
-      # create a spawn boundary
+      #  # create a spawn boundary
       # sb = SpawnBoundary([self.boundary])
       # sb.sample(
       #   self.grasp_target, 
@@ -60,7 +73,9 @@ class VisionStatic(Task):
       #   max_rotation = (0, 0, 0)
       # ) 
       
-      return [f"simple grasping task test, to try out grasping"]
+      return [f"Vision_Static desc"]
+    
+    ## revert back to original size
     def cleanup(self) -> None:
       inv = 1. / self.picked_scale
       self.grasp_target_visual.scale_object(
