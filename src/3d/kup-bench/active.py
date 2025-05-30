eee195816711028ceda367607f36742799ddd737
Kagan Ugur Pekgoz <kaganpekgoz@gmail.com>
2025-05-30 17:30:26 +0100

    making an active version fo rwd and plan 1 agent

---
+++ src/3d/kup-bench/active.py
@@ -0,0 +1,216 @@
+#%%
+## 1. Initalise and Import
+import numpy as np
+from rlbench.action_modes.action_mode import MoveArmThenGripper
+from rlbench.action_modes.arm_action_modes import JointVelocity, JointPosition
+from rlbench.action_modes.gripper_action_modes import Discrete
+from rlbench.environment import Environment
+from rlbench.observation_config import ObservationConfig, CameraConfig
+
+# Tasks
+## No Obstacle
+from rlbench.tasks.reach_target_no_obs_side_r import ReachTargetNoObsSideR as ReachNoObs_SideR
+from rlbench.tasks.reach_target_no_obs_side_l import ReachTargetNoObsSideL as ReachNoObs_SideL
+from rlbench.tasks.reach_target_no_obs_central import ReachTargetNoObsCentral as ReachNoObs_Central
+from rlbench.tasks.reach_target_no_obs_random import ReachTargetNoObsRandom as ReachNoObs_PlaceRandom
+## Obstacle
+from rlbench.tasks.reach_target_obs_static_left import ReachTargetObsStaticLeft as ReachObs_StaticLeft
+from rlbench.tasks.reach_target_obs_static import ReachTargetObsStatic as ReachObs_Static 
+from rlbench.tasks.reach_target_obs_random_static import ReachTargetObsRandomStatic as ReachObs_RandomStatic
+from rlbench.tasks.reach_target_obs_random import ReachTargetObsRandom as ReachObs_Random
+from rlbench.tasks.reach_target_obs_ind_random import ReachTargetObsIndRandom as ReachObs_IndepRandom
+## Grasp
+from rlbench.tasks.simple_grasp import SimpleGrasp as Grasp_Simple
+from rlbench.tasks.grasp_and_move import GraspAndMove as Grasp_ThenMove
+## Vision Experiments - Grasp
+from rlbench.tasks.vision_static import VisionStatic as Vision_Static
+from rlbench.tasks.vision_random import VisionRandom as Vision_Random
+
+from rlbench.backend.observation import Observation
+from rlbench.demo import Demo
+
+from pyrep.const import RenderMode
+from pyrep.objects import Object, VisionSensor, Shape
+
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+from lib.agent import Agent
+# from modules.active_agent import ActiveAgent_Plan1 as ActiveAgent
+
+import matplotlib.pyplot as plt
+from lib.cam_type import CamType
+from lib.policy_type import PolicyType
+
+from lib.utils import get_task_name, now
+from seed import set_seed
+from pprint import pprint
+
+set_seed()
+
+#%%
+## 2. Create Environment and Set Model Name
+# To use 'saved' demos, set the path below, and set live_demos=False
+
+live_demos = False
+DATASET = '' if live_demos else 'data/demos20-ReachObs_Random'
+
+obs_config = ObservationConfig()
+obs_config.set_all(True) ## important to get the data from the joints etc
+cam_config = CameraConfig(rgb=True, depth=True, mask=True, point_cloud=True,
+                              render_mode=RenderMode.OPENGL,
+                    image_size=(64, 64))
+nocam_config = CameraConfig(rgb=False, depth=False, mask=False,
+                          render_mode=RenderMode.OPENGL)
+
+obs_config.right_shoulder_camera = cam_config
+obs_config.left_shoulder_camera = cam_config
+obs_config.wrist_camera = cam_config
+
+
+## added cam config to be able to save demos
+obs_config.overhead_camera = cam_config
+obs_config.front_camera = cam_config 
+
+## TODO: start with Joint Position control then go to velocity if nedeed
+# action_mode = MoveArmThenGripper(
+#     arm_action_mode=JointPosition(absolute_mode=True), 
+#     gripper_action_mode=Discrete()
+# )
+action_mode = MoveArmThenGripper(
+    arm_action_mode=JointVelocity(), 
+    gripper_action_mode=Discrete()
+)
+
+env = Environment(
+    action_mode, DATASET, obs_config, False)
+ 
+env.launch()
+
+#%%
+## 3. Attach Task and create Agent
+## NOTE: importing cv2 before lauunching the env crashes the system??
+from modules.active_agent import ActiveAgent_Plan1 as ActiveAgent
+pol_type = PolicyType.DEPTH_GRASP
+
+cam_type = CamType.WRIST | CamType.WRIST_DEPTH
+
+# task = Vision_Random
+task = ReachNoObs_Central
+print(env.get_task.__code__.co_filename)
+
+# task_env = env.get_task(task_class = task, **task_params)
+task_env = env.get_task(task_class = task)
+target_name = "target"
+
+  
+wd = CamType.WRIST | CamType.WRIST_DEPTH
+all_cams = CamType.LEFT_SHOULDER | CamType.RIGHT_SHOULDER | CamType.WRIST_DEPTH
+
+## use best il agent
+il_agent = Agent(
+  env.action_shape[0],
+  policy_type=PolicyType.RNN_GRASP,
+  cam_type = wd,
+  rnn_opts = {
+    "config": "depth_feats",
+    # "attn_opts": {"is_deep_fuse": True},
+    "use_proprio": False
+  }
+)
+
+agent = ActiveAgent(
+  env.action_shape[0],
+  il_agent = il_agent, 
+  cam_type = wd, # doesnt really matter
+  vis_thresh= 0.02
+)
+
+model_name = f"rwd-{get_task_name(task)}-{il_agent}--{now()}"
+model_path = f"./all-models/reach-with-demos/{model_name}.pth"
+print(model_name)
+agent
+#%%
+demos: list[Demo] = task_env.get_demos(1, live_demos=True)
+demos
+
+#%%
+## 5. Train
+training_params = {
+  "epochs": 1000,
+  "minibatch_size": 10,
+  "lr": 1e-3,
+  "shuffle_obs_in_demo": False,
+  "shuffle_data": True,
+  "lock_loader_seed": 1,
+  "dataset_to_use": "demo",
+  "lambda_grasp_loss": 1,
+  "data_label": "joint_velocities"
+}
+
+ingest_num = 10
+
+print(f"-> Using {ingest_num} demos")
+
+il_agent.ingest(demos[:ingest_num], **training_params) ## trains here
+il_agent.save_model(model_path)
+#%%
+demos[0][0].joint_positions
+#%%
+string = torch.load("all-models/reach-with-demos/rwd-Reach_Central-agent-policy:rnn_grasp_policy-cams:wrist+wrist_depth-policy:rnn_grasp_policy-rnn_opts:{'config': 'depth_feats', 'use_proprio': False}--_May29_12-29.pth")
+il_agent.policy.load_state_dict(string)
+#%%
+_, obs = task_env.reset()
+il_agent.policy.to("cpu")
+# for demo in test_demos:
+#   _, obs = task_env.reset_to_demo(demo)
+
+count = 0
+done = False
+distances = []
+while not done:
+  obs: Observation
+  
+  action, rets = il_agent.act(obs)
+  # rgb_out = rets["rgb_fused"]
+  # rgb_unfsuedout = rets["rgb_unfused"]
+  # depth_out = rets["depth_fused"]
+
+  # plt.imshow(obs.wrist_depth)
+  # plt.imshow(obs.wrist_rgb)
+  # plot_featmap(rgb_out, "RGB feats (fused w/ attn)")
+  # plot_featmap(rgb_unfsuedout, "RGB feats (not fused)")
+  # plot_featmap(depth_out)
+
+  action = action.squeeze(0)
+  # print(f"{action.shape =}")
+  # print(f"{action[-1] =}")
+  # print(f"{action.shape = }")
+  obs, reward, done = task_env.step(action)
+  gripper = Object.get_object("Panda_gripper")
+  target = Object.get_object("target")
+
+  
+  
+  # vis_score = check_visibility("cam_wrist", "target")
+  
+  # print(f"{vis_score = }")
+  
+  
+  distance = np.linalg.norm(gripper.get_position() - target.get_position())
+  distances.append(distance)
+  # print(f"{done = }")
+  
+  count += 1
+  if count == 100:
+    break
+  
+print(f"Done Successfull! done in {count} steps" if done else "Failed!")
+print(f"Final distance: {distances[-1]}")
+
+#%%
+task_env = env.get_task(ReachObs_Random)
+agent.act(task_env.reset()[1], task_env)
+#%%
+
---
+++ src/3d/kup-bench/modules/active_agent.py
@@ -0,0 +1,262 @@
+from typing import Literal, Optional
+import numpy as np
+import torch
+import cv2
+from rlbench.backend.robot import Robot
+from rlbench.task_environment import TaskEnvironment
+from rlbench.backend.observation import Observation
+
+from scipy.spatial.transform import Rotation as rot
+
+from pyrep.errors import ConfigurationError
+
+from lib.agent import Agent
+from lib.cam_type import CamType
+from lib.utils import pick_obs_from_cam
+
+class ActiveAgent_Plan1:
+  def __str__(self) -> str:
+    return f"active-agent-plan1:il-agent:{self.il_agent}"
+  def __repr__(self) -> str:
+    return f"ActiveAgent_Plan1(il_agent={repr(self.il_agent)})"
+  
+  def __init__(
+    self, 
+    action_dim: int, 
+    il_agent: Agent,
+    cam_type: CamType = CamType.WRIST | CamType.WRIST_DEPTH, ## rgb and point cloud esentially
+    vis_thresh: float = 0.5,
+  ):
+    self.action_dim = action_dim
+    self.cam_type = cam_type ## not really used
+    self.il_agent = il_agent
+    self.vis_thresh = vis_thresh
+    
+    self.best = {"vis_score": -1, "joint_pos": None }
+    
+
+  def _set_new_best(self, score: float, pose: np.ndarray): 
+    self.best["vis_score"] = score
+    self.best["joint_pos"] = pose
+  
+  def _get_best_joint_pos_action(self) -> Optional[np.ndarray]:
+    return self.best["joint_pos"]
+
+  ## TODO: this can combine multuple different things down the line
+    # s_coverage = |target_pc| / max(|pc_seen_historcial|) to normalise for object size
+    # proj_area = mask.sum() / |mask|
+    ## spread = np.mean(np.linalg.norm(target_pc - centroid, axis = 1))
+    # np.linalg.ei
+  def visibility_score(self, 
+    obs: Observation, ## NOTE: VisionSensor with no 'rel_to' param gives in terms of world
+    hsv_low: np.ndarray = np.array([0, 177, 0]),
+    hsv_high: np.ndarray = np.array([179, 255, 255]),
+  ):
+    ## change if trying other cams? dont think so tho
+    rgb = obs.wrist_rgb
+    world_pc = obs.wrist_point_cloud
+
+    ## possible issues, object size per view, the object may be far
+    # centroid = target_pc.mean(axis=0)
+    target_pc, mask = self._get_masked_pc_and_mask(rgb, world_pc, hsv_low, hsv_high)
+
+    ## currently a simple colour checking in the area
+    
+    return mask.sum() / mask.size ## h * w
+    
+  # def get_joint_velocity(curr, target, )
+  ## NOTE: play around with 
+
+  def _get_masked_pc_and_mask(self, 
+    rgb: np.ndarray, 
+    world_pc: np.ndarray, ## NOTE: VisionSensor with no 'rel_to' param gives in terms of world
+    hsv_low: np.ndarray,
+    hsv_high: np.ndarray,
+  ) -> tuple:
+    ## the vision_sensor gives the point cloud in world frame
+    hsv = hsv = cv2.cvtColor(rgb, cv2.COLOR_RGB2HSV)
+    mask = cv2.inRange(hsv, hsv_low, hsv_high) ## mention in report how this cam to be
+    mask = mask / 255 ## above gives in range [0, 255]
+
+    pc_flat = world_pc.reshape(-1, 3)
+    mask_flat = mask.reshape(-1).astype(bool)
+
+    masked_pc = pc_flat[mask_flat]
+    return masked_pc, mask
+
+  def _sample_camera_poses(self, 
+    ee_pos,
+    n_samples: int, 
+    radius: float = 0.005,
+  ):
+    poses = []
+    for _ in range(n_samples):
+      ## get position
+      # uniform sampling on sphere surface around gripper pose
+      theta = np.arccos(2 * np.random.rand() - 1)
+      phi   = 2 * np.pi * np.random.rand()
+      x_off = radius * np.sin(theta) * np.cos(phi)
+      y_off = radius * np.sin(theta) * np.sin(phi)
+      z_off = radius * np.cos(theta)
+      off = np.array([x_off, y_off, z_off])
+      pos = ee_pos + off
+
+
+      ## get rotation
+      ## TODO add math for this in the report
+      # forward = ee_pos - pos ## same as - off
+      forward = -off
+      forward /= np.linalg.norm(forward)
+      world_up = np.array([0.0, 0.0, 1.])
+      right = np.cross(world_up, forward)
+
+      ## handle case when up ~= forward
+      if np.linalg.norm(right) < 1e-6:
+          world_up = np.array([1.0, 0.0, 0.0])
+          right = np.cross(world_up, forward)
+      right /= np.linalg.norm(right)
+      true_up = np.cross(forward, right)
+
+      R_mat = np.stack([right, true_up, forward], axis=1)  # 3 x 3 rot
+      quat = rot.from_matrix(R_mat).as_quat()  
+
+      pose = np.zeros(7, dtype=float) ## xyz + quat
+      pose[0:3] = pos
+      pose[3:7] = quat
+      poses.append(pose)
+
+    return poses
+  
+  ## returns the joint positions to achieve pose: (7, )
+  def _solve_ik(self, robot: Robot, pose: np.ndarray) -> Optional[np.ndarray]:
+    try: 
+      return robot.arm.solve_ik_via_sampling(
+        position=pose[:3], # (x, y, z)
+        quaternion=pose[3:] # (qx, qy, qz, qw)
+      )
+    except ConfigurationError as err:
+      print(f"[active_agent (solve_ik)] Error: '{err}', continuing to sample")
+      return None
+      
+
+  def act_il(self, 
+    obs: Observation,
+    task_env: TaskEnvironment,
+    max_eplen: Optional[int] = None
+  ) -> tuple[Observation, int, bool]: ##obs, reward, done
+    done = False
+    count = 0
+
+    while not done:
+      action, rets = self.il_agent.act(obs)
+      action = action.squeeze(0)
+
+      obs, _, done = task_env.step(action)
+      if done:
+        self.state = "done"
+        return obs, _, done
+
+      vis_score = self.visibility_score(obs)
+      print(f"il - {vis_score = }")
+      if vis_score < self.vis_thresh:
+        self.state = "active"
+        return obs, _, done
+      
+      if max_eplen is not None:
+        if count >= max_eplen:
+          self.state = "done"
+          return obs, _, False ## not done fail this iteration get to active
+        count += 1
+      
+  def act_active(self, 
+    obs: Observation,
+    task_env: TaskEnvironment, 
+    pose_samples: int
+  ) -> tuple[Observation, int, bool]: ##obs, reward, done
+    robot = task_env._robot
+
+    vis_score = self.visibility_score(obs)
+
+    if vis_score >= self.vis_thresh:
+      return obs, 0, False ## not sure placeholder values, i gueuss this sohuld do act_il?
+      
+
+    for pose in self._sample_camera_poses(
+      robot.gripper.get_position(), 
+      pose_samples
+    ):
+      ## finds the joint positions to get this pose
+      jpos_action = self._solve_ik(robot, pose)
+
+      if jpos_action is None:
+        continue ## check next sample
+
+      print(f"act_active {jpos_action.shape = } (from solve_ik)")
+      jpos_action = jpos_action.squeeze(0)
+
+      action = np.append(jpos_action, [0.]) ## add the gripper move, dont care about it
+
+      print(f"act_active {jpos_action.shape = } (squeezed)")
+      
+      obs, _, done = task_env.step(action) ## step to new pose
+      if done: 
+        self.state = "done"
+        return obs, _ , done
+
+      s = self.visibility_score(obs)
+      print(f"act_active - {vis_score = }")
+
+      if s > self.best["vis_score"]:
+        self._set_new_best(s, action)
+
+    if self._get_best_joint_pos_action() is None:
+      raise NotImplementedError("[active_agent - act] No better pose found, but I cant jsut give up here, needs to do something")
+    ## otherwise move to best:
+    ## when velocities get involved this may need more calculations
+
+    return  task_env.step(self._get_best_joint_pos_action())
+    ## cant be None here
+
+  ## full action loop unlike the other agents
+  ## call as .act(task_env.reset()[1], task_env)
+  def act(self, 
+    init_obs: Observation,
+    task_env: TaskEnvironment,
+    pose_samples: int = 20,
+    max_loops: int = 200
+  ) -> bool:
+    
+    done = False    
+    obs = init_obs
+    vis_score = self.visibility_score(obs)
+    print(f"(start) act - {vis_score = }")
+
+    if vis_score >= self.vis_thresh:
+      self.state = "il"
+    else:
+      self.state = "active"
+
+    ## look for a better pose until it finds a good enogh one to start il
+    for _ in range(max_loops):
+      if self.state == "active":
+        obs, _ , done = self.act_active(obs, task_env, pose_samples)
+
+        if done: 
+          self.state = "done"
+          break
+
+        vis_score = self.visibility_score(obs)
+        print(f"active - {vis_score = }")
+        
+        if vis_score >= self.vis_thresh:
+          self.state = "il"
+      
+      if self.state == "il":
+        ## act il should only return if the vis score drops significantly
+        obs, _ , done = self.act_il(obs, task_env)
+        if done: 
+          self.state = "done"
+          break
+
+    return done
+
