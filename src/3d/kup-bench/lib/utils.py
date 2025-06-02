7f0892d8dd3602757db222859b5fbb0afce7c1d4
Kagan Ugur Pekgoz <kaganpekgoz@gmail.com>
2025-06-02 16:11:05 +0100

    adding new cams to get, so no error

--- src/3d/kup-bench/lib/utils.py
+++ src/3d/kup-bench/lib/utils.py
@@ -77,6 +77,10 @@ def pick_obs_from_cam(cam_type: CamType, obs: Observation, normalise_rgb: bool =
       return (obs.left_shoulder_rgb / 255) if normalise_rgb else obs.left_shoulder_rgb
     case CamType.RIGHT_SHOULDER:
       return (obs.right_shoulder_rgb / 255) if normalise_rgb else obs.right_shoulder_rgb
+    case CamType.OVERHEAD:
+      return (obs.overhead_rgb / 255) if normalise_rgb else obs.right_shoulder_rgb
+    case CamType.FRONT:
+      return (obs.front_rgb / 255) if normalise_rgb else obs.right_shoulder_rgb
     ## NOTE: depth normalisation is min-max here, if we want to keep the meanings of metres in the model, maybe use log etc??
     case CamType.WRIST_DEPTH:
       depth_arr = ndarray_min_max_norm(obs.wrist_depth) if normalise_depth else obs.wrist_depth 
