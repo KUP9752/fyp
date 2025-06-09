542d96d37d945597261587d3cf90857a232bf030
Kagan Ugur Pekgoz <kaganpekgoz@gmail.com>
2025-06-09 20:49:13 +0100

    final fixes before last runs

--- src/3d/kup-bench/modules/cnns/fusing_encoder.py
+++ src/3d/kup-bench/modules/cnns/fusing_encoder.py
@@ -17,7 +17,7 @@ from modules.cnns.cnn_encoder import CNNEncoder
 from modules.cnns.multi_cam_cnn import MultiCamCnn
 from modules.cnns.vit_encoder import MultiViewEncoder
 
-from modules.film_net import FilmModulator
+from modules.film_net import FilmModulator, DeepEncoder
 from modules.cnns.cross_attn_feats import CrossAttentionFeatures
 
 from modules.joint_pos_encoder import JointPosEncoder
@@ -171,9 +171,10 @@ class FusingEncoder(nn.Module):
         self.film = FilmModulator(
           in1 = 3, ## wrist rgb, 
           in2 = 1, ## wrist d, 
+          feat_dim = 6,
           do_both = False,
         )
-        self.mod_downer = CNNEncoder(64, layers = self.opts["film"]["downer_cnn_layers"]) ## use default layers
+        self.mod_downer = CNNEncoder(6, layers = self.opts["film"]["downer_cnn_layers"]) ## use default layers
         ##NOTE: modulation happens at the resolution level need to downsample
 
         self.feat_size = self.mod_downer.flat_out_size
@@ -186,9 +187,10 @@ class FusingEncoder(nn.Module):
         self.film = FilmModulator(
           in1 = 1, ## wrist d, 
           in2 = 3, ## wrist rgb, 
+          feat_dim = 6,
           do_both = False,
         )
-        self.mod_downer = CNNEncoder(64, layers = self.opts["film"]["downer_cnn_layers"]) ## use default layers
+        self.mod_downer = CNNEncoder(6, layers = self.opts["film"]["downer_cnn_layers"]) ## use default layers
         self.feat_size = self.mod_downer.flat_out_size
 
       case FuseConfig.Wfilm_Dfilm:
@@ -199,10 +201,42 @@ class FusingEncoder(nn.Module):
         self.film = FilmModulator(
           in1 = 3, ## wrist rgb, 
           in2 = 1, ## wrist d, 
+          feat_dim = 6,
           do_both = True,
         )
-        self.mod_downer = CNNEncoder(128, layers = self.opts["film"]["double_downer_cnn_layers"]) ## double the size of last time, 2 of them
+        self.mod_downer = CNNEncoder(12, layers = self.opts["film"]["double_downer_cnn_layers"]) ## double the size of last time, 2 of them
         self.feat_size = self.mod_downer.flat_out_size
+      case (
+        FuseConfig.Wfilm_D_LATE |
+        FuseConfig.W_Dfilm_LATE |
+        FuseConfig.Wfilm_Dfilm_LATE 
+      ):
+        self._check_cam(needed=CamType.wrists())
+        self._fail_if_cam(CamType.shoulders())
+
+        self.enc_w = DeepEncoder(3)## 4 -> 8 -> 16, 8, 8
+        self.enc_d = DeepEncoder(1)## 4 -> 8 -> 16, 8, 8
+
+        if self.config == FuseConfig.Wfilm_Dfilm_LATE:
+          self.film = FilmModulator(
+            in1 = 16, 
+            in2 = 16, 
+            feat_dim = 16,
+            do_both = True
+          )
+          self.feat_size = self.enc_w.flat_out_size + self.enc_d.flat_out_size ## only one is needed
+        else:
+          self.film = FilmModulator(
+            in1 = 16, 
+            in2 = 16, 
+            feat_dim = 16,
+            do_both = False
+          )
+          self.feat_size = self.enc_w.flat_out_size ## only one is needed
+        # if self.config == FuseConfig.Wfilm_D_LATE:
+        # if self.config == FuseConfig.W_Dfilm_LATE:
+
+          
 
       case FuseConfig.W_D_L_R:
         self.multi_enc = MultiCamCnn(self.cam_type)
@@ -216,22 +250,23 @@ class FusingEncoder(nn.Module):
         # force all 4, cant be asked to figure it out for 3 no real point for 3
         self._check_cam(needed=CamType.main4())
         ## the order of fusing is w+d then l+r then wd+lr
-        ##TODO modulate wrist rgb with depth, left and right with each other
 
         self.wd_film = FilmModulator(
           in1 = 3, 
           in2 = 1, 
+          feat_dim = 6,
           do_both=False
         )
-        self.wd_downer = CNNEncoder(64, layers = self.opts["film"]["downer_cnn_layers"])
+        self.wd_downer = CNNEncoder(6, layers = self.opts["film"]["downer_cnn_layers"])
 
         self.lr_film = FilmModulator(
           in1 = 3, 
           in2 = 3, 
+          feat_dim = 3,
           do_both=True
         )
 
-        self.lr_downer = CNNEncoder(128, layers = self.opts["film"]["double_downer_cnn_layers"])
+        self.lr_downer = CNNEncoder(6, layers = self.opts["film"]["double_downer_cnn_layers"])
 
         self.feat_size = self.lr_downer.flat_out_size + self.wd_downer.flat_out_size
 
@@ -331,6 +366,7 @@ class FusingEncoder(nn.Module):
 
         if self.config == FuseConfig.W_Dfilm:
           mod = self.film(dX, wX)
+          
         elif self.config == FuseConfig.Wfilm_D:
           mod = self.film(wX, dX)
         else:# self.config == FuseConfig.Wfilm_Dfilm:
@@ -357,6 +393,47 @@ class FusingEncoder(nn.Module):
         cated = torch.cat(feats, dim = 1)
         return cated, ret_dict
       
+      case (
+        FuseConfig.Wfilm_D_LATE |
+        FuseConfig.W_Dfilm_LATE |
+        FuseConfig.Wfilm_Dfilm_LATE 
+      ):
+        self._check_cam(needed=CamType.wrists())
+        wX = image[:, :3, :, :]
+        dX = image[:, -1, :, :].unsqueeze(dim=1) 
+
+        f_w = self.enc_w(wX)
+        f_d = self.enc_d(dX)
+
+        if self.config == FuseConfig.W_Dfilm_LATE:
+          feats = self.film(f_d, f_w)
+        elif self.config == FuseConfig.Wfilm_D_LATE:
+          feats = self.film(f_w, f_d)
+        else:# self.config == FuseConfig.Wfilm_Dfilm:
+          mod1, mod2 = self.film(f_w, f_d)
+          feats = torch.cat([mod1, mod2], dim = 1)
+
+        return feats, ret_dict
+
+      case FuseConfig.W_D_L_R:
+        feats = []
+        curr_index = 0
+
+        for ct in CamType.main3():
+          if ct & self.cam_type:
+            feat = self.multi_enc(image[:, curr_index:curr_index+3, :, :], ct)
+            feats.append(feat)
+            curr_index +=3
+
+        if self.cam_type & CamType.WRIST_DEPTH:
+          feat = self.multi_enc(image[:, -1, :, :].unsqueeze(dim=1), CamType.WRIST_DEPTH)
+          feats.append(feat)
+
+        cated = torch.cat(feats, dim = 1)
+        return cated, ret_dict
+
+
+
       case FuseConfig.W_D_L_R_FILM:
         self._check_cam(needed=CamType.main4())
         ## all must be here
