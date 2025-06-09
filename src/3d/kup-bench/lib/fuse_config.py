64729a3b3ac87696e31e23a78d0fdca1c33c9819
Kagan Ugur Pekgoz <kaganpekgoz@gmail.com>
2025-06-09 14:44:45 +0100

    removed exttra comments

--- src/3d/kup-bench/lib/fuse_config.py
+++ src/3d/kup-bench/lib/fuse_config.py
@@ -22,5 +22,5 @@ class FuseConfig(Enum):
 
   #w + d + l + r
   W_D_L_R = auto() ## all separated but cated before linear layer
-  W_D_L_R_FILM = auto() ## all separated but cated before linear layer
+  W_D_L_R_FILM = auto() ## 
   W_D_L_R_ATTN = auto() #' 4 way cross attention?
